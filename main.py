ded78b045ec79f0944a9d23319372efb57c27ec4
svyatoslav123 <151392816+svyatoslav123@users.noreply.github.com>
2024-05-02 17:03:30 +0300

    cup

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import pygame
+from player1 import Player
 
-
-
+pygame.init()
 
 window = pygame.display.set_mode((700, 500))
 fps = pygame.time.Clock()
+pygame.mixer.init()
+
+sound = pygame.mixer.music.load("07 Botanic Panic.mp3")
+pygame.mixer.music.play(-5)
+
+
+
 
 background = pygame.transform.scale(
     pygame.image.load("HD-wallpaper-video-game-cuphead.jpg"), (800, 500)
 )
-
+cuphead = Player(20, 420, 80, 80, "CupheadCharacter (1).webp", 5)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -19,6 +26,7 @@ while True:
 
 
     window.blit(background, (0, 0))
-
+    cuphead.move()
+    cuphead.draw(window)
     pygame.display.flip()
     fps.tick(60)
